254040f39fca887ce162655f425d39aa20544650
Danilo Paulino da Silvar <danilopaulinodasilva@gmail.com>
2020-02-08 20:00:47 -0300

    db:migrate realizada

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
     dialect: 'postgres',
     host: 'localhost',
     username: 'postgres',
-    password: 'docker',
+    password: 'MyNewPass',
     database: 'gobarber',
     define: {
         timestamps: true,
