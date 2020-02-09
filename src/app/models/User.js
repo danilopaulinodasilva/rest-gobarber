0ff50f321fbed0fb75b0ca5b246fab9c43ddf224
Danilo Paulino da Silvar <danilopaulinodasilva@gmail.com>
2020-02-09 07:19:40 -0300

    Criação do model User

---
+++ src/app/models/User.js
@@ -0,0 +1,16 @@
+const { Model } = require('sequelize');
+
+class User extends Model {
+    static init(sequelize) {
+        super.init({ // super acessa o método da classe pai, init() dentro de Model
+            name: Sequelize.STRING,
+            email: Sequelize.STRING,
+            password_hash: Sequelize.STRING,
+            provider: Sequelize.BOOLEAN,
+        },{
+            sequelize
+        }); 
+    }
+}
+
+export default User;
