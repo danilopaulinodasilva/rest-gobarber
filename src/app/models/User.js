aaeca50ccd6bf1537c5b175280cc1e5aa7e19671
Danilo Paulino da Silvar <danilopaulinodasilva@gmail.com>
2020-02-09 21:47:53 -0300

    Adicionado hash na senha com bcrypt

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,16 +1,26 @@
 const Sequelize = require('sequelize');
 const { Model } = require('sequelize');
+const bcrypt = require('bcryptjs');
 
 class User extends Model {
     static init(sequelize) {
         super.init({ // super acessa o método da classe pai, init() dentro de Model
             name: Sequelize.STRING,
             email: Sequelize.STRING,
+            password: Sequelize.VIRTUAL,
             password_hash: Sequelize.STRING,
             provider: Sequelize.BOOLEAN,
         },{
             sequelize // passa a conexão
         }); 
+
+        this.addHook('beforeSave', async user => {
+            if (user.password) {
+              user.password_hash = await bcrypt.hash(user.password, 8);
+            }
+          });
+
+        return this;
     }
 }
 
