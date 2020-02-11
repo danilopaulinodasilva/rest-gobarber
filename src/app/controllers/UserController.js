e4050741013370a12d76d32202fc3f2839578e6f
Danilo Paulino da Silvar <danilopaulinodasilva@gmail.com>
2020-02-11 07:08:13 -0300

    Criação do metodo update no UserController

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -27,7 +27,41 @@ class UserController {
     }
 
     async update(req,res) {
-        return res.json({ ok: true })
+
+        const { email, oldPassword } = req.body;
+
+        // busca o usuario dentro do banco com a função findByPk (find by primary key) no caso id que vem do middleware no req.userId
+        // e retorna um objeto completo com todos dados encontrados no database, para ser usado daqui pra frente como: user
+        const user = await User.findByPk(req.userId);
+
+        // caso ele queira mudar, verifica se o email já não existe no banco
+        if(email !== user.email) {
+
+            // procura o email usando findOne
+            const userExists = await User.findOne({ where: { email: req.body.email } });
+
+            // se existir retorna que o email já existe
+            if(userExists) {
+                return res.status(400).json({ error: 'Email already existis' });
+            }
+        }
+
+        // se o usuário passou o oldPassword e se a senha antiga realmente bate com a senha do database
+        if (oldPassword && !(await user.checkPassword(oldPassword))) {
+            return res.status(401).json({ error: 'Password does not match' })
+        }
+        
+        // se tudo der certo acima, faz o update do usuario através do model
+        const { id, name, provider } = await user.update(req.body);
+
+        // retorna os campos definidos acima
+        return res.json({
+            id,
+            name,
+            email,
+            provider
+        });
+
     }
 
 }
